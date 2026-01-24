91af60ddea6d12ef99f7a05eb6977c62c42cf73d
Andrew Ganzha <jackbrikman@mail.ru>
2026-01-24 09:26:06 +0300

    Small fixes for methods

--- app/routers/products.py
+++ app/routers/products.py
@@ -38,14 +38,14 @@ async def create_product(product: ProductCreate, db: AsyncSession = Depends(get_
     db_product = ProductModel(**product.model_dump())
     db.add(db_product)
     await db.commit()
-    db.refresh(db_product)
+    await db.refresh(db_product)
 
     return db_product
 
 
 @router.get("/category/{category_id}")
 async def get_products_by_category(category_id: int, db: AsyncSession = Depends(get_async_db)):
-    result_category = await db.scalars(select(CategoryModel).where(CategoryModel.id == category_id))
+    result_category = await db.scalars(select(CategoryModel).where(CategoryModel.id == category_id, CategoryModel.is_active == True))
     category = result_category.first()
 
     if category is None:
@@ -64,6 +64,15 @@ async def get_product(product_id: int, db: AsyncSession = Depends(get_async_db))
 
     if product is None:
         raise HTTPException(status_code=404, detail="Product not found")
+
+    category_result = await db.scalars(
+        select(CategoryModel).where(CategoryModel.id == product.category_id,
+                                    CategoryModel.is_active == True)
+    )
+    category = category_result.first()
+    if not category:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Category not found or inactive")
 
     return product
 
