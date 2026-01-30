58953c833add73c328a2a90edb82e6e44204f17f
Andrew Ganzha <jackbrikman@mail.ru>
2026-01-30 15:30:43 +0300

    Add auth method for adding refresh token

--- app/database.py
+++ app/database.py
@@ -1,12 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-# Строка подключения для SQLite
-DATABASE_URL = "sqlite:///ecommerce.db"
+# Строка подключения для SQLite (sync)
+SYNC_DATABASE_URL = os.getenv("SYNC_DATABASE_URL", "sqlite:///ecommerce.db")
 
 # Создаём Engine
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(SYNC_DATABASE_URL, echo=True)
 
 # Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
@@ -17,11 +19,14 @@ SessionLocal = sessionmaker(bind=engine)
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-# Строка подключения для PostgreSQl
-DATABASE_URL = "postgresql+asyncpg://shop:shop@localhost:5432/shop"
+# Строка подключения для PostgreSQL (async)
+ASYNC_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://shop:shop@db:5432/shop",
+)
 
 # Создаём Engine
-async_engine = create_async_engine(DATABASE_URL, echo=True)
+async_engine = create_async_engine(ASYNC_DATABASE_URL, echo=True)
 
 # Настраиваем фабрику сеансов
 async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
