1f3617ca7501360cfef2f4112274b644e1c53041
Andrew Ganzha <jackbrikman@mail.ru>
2026-01-30 16:48:55 +0300

    Update migrations, fix database settings

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -9,8 +9,8 @@ class Review(Base):
     __tablename__ = "reviews"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", nullable=False))
-    product_id: Mapped[int] = mapped_column(ForeignKey("products.id", nullable=False))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
     comment: Mapped[str] = mapped_column(String, nullable=True)
     comment_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
     grade: Mapped[int] = mapped_column(Integer, nullable=True)
